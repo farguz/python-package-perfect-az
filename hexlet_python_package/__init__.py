e288bd8702816bd43940a6e04d1d1c3ff816e9ae
aleksei z <farguzalexey@gmail.com>
2024-12-30 14:25:43 +0300

    add __all__ to init.py

---
+++ hexlet_python_package/__init__.py
@@ -0,0 +1,7 @@
+import hexlet_python_package.half as my_lib
+from hexlet_python_package.half import half as half
+
+__all__ =(
+    'my_lib',
+    'half'
+)
--- hexlet_python_package/half.py
+++ hexlet_python_package/half.py
@@ -1,2 +1,10 @@
+from more_itertools import sliced, substrings
+
 def half(num):
     return num / 2
+
+subs = [''.join(s) for s in substrings('more')]
+print(subs)
+
+slices = list(sliced((1, 2, 3, 4, 5, 6, 7, 8), 3))
+print(slices)
